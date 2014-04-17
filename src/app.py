cf074c332dba6edbb173925e83cf3c96b0c27702
SliceOLife <me@jordify.nl>
2014-04-17 18:46:24 +0200

    Config loading

--- src/app.py
+++ src/app.py
@@ -1,36 +1,45 @@
-from dropboxService import auth
-from dropboxService import sync
-
 import argparse
 import glob
-import config
-from modules.utils import writeLog
+
+from dropboxService import auth
+from dropboxService import sync
+from modules.utils import writeLog, loadPlugin, loadConf, setupLog
+
 
 
 # Parse arguments
 parser = argparse.ArgumentParser(description='WallCore command line options')
-parser.add_argument('-d', help='Debug switch for development', action='store_true')
+#parser.add_argument('-d', help='debug switch for development', action='store_true')
+parser.add_argument('-conf', help='set config file to load', dest='conf')
 args = parser.parse_args()
 
+if args.conf is not None:
+  config = loadConf(args.conf)
+else:
+  config = loadConf('default.conf')
+
+# Setup logging
+setupLog()
 
 # Load-up (default) plugins
-pluginsAvailable = glob.glob("plugins/*.py")
-pluginsLoaded = {}
-for plugin in pluginsAvailable:
-  writeLog('i', 'Found plugin: %s' % (plugin))
+# pluginsAvailable = glob.glob("plugins/*.py")
+# pluginsLoaded = {}
+# for plugin in pluginsAvailable:
+#     writeLog('i', 'Found plugin: %s' % (plugin))
+#     loadPlugin(plugin)
 
 # Dropbox auth
-if args.d:
-  syncService = sync(config.DROPBOX_DEBUGKEY)
+if config['DROPBOX_DEBUGKEY'] is not None:
+    syncService = sync(config['DROPBOX_DEBUGKEY'])
 else:
-  authService = auth(config.DROPBOX_KEY, config.DROPBOX_SECRET)
-  print("Please navigate to: " + authService.getAuthURL())
-  user_code = raw_input("Please enter Dropbox' response code")
-  flowAuth = authService.finishFlow(user_code)
-  writeLog('d', flowAuth)
-  syncService = sync(flowAuth)
+    authService = auth(config['DROPBOX_KEY'], config['DROPBOX_SECRET'])
+    print("Please navigate to: " + authService.getAuthURL())
+    user_code = raw_input("Please enter Dropbox' response code")
+    flowAuth = authService.finishFlow(user_code)
+    writeLog('d', flowAuth)
+    syncService = sync(flowAuth)
 
 uid = syncService.getUserInfo()['uid']
 name = syncService.getUserInfo()['display_name']
 writeLog('i', "Logged into %s[%s]'s Dropbox " % (name, uid))
-writeLog('i', 'Using WallCore v%s' % (config.APP_VER))
+writeLog('i', 'Using WallCore v%s' % (config['APP_VER']))
--- src/modules/utils.py
+++ src/modules/utils.py
@@ -1,52 +1,73 @@
 import hashlib
 import logging
-import config
-import os
 import imp
 
-if(config.APP_DEBUG == True):
-  logging.basicConfig(format='%(levelname)s:%(message)s',filename='wallcore.log', level=logging.DEBUG)
-else:
-  logging.basicConfig(format='%(levelname)s:%(message)s',filename='wallcore.log',level=logging.WARNING)
+import config
+import os
 
+config = {}
+
+def setupLog():
+  if (config['APP_DEBUG'] == True):
+      logging.basicConfig(format='%(levelname)s:%(message)s', filename='wallcore.log', level=logging.DEBUG)
+  else:
+      logging.basicConfig(format='%(levelname)s:%(message)s', filename='wallcore.log', level=logging.WARNING)
 
 def MD5(fn):
-  with open(file_name, 'rb') as file_to_check:
-    data = file_to_check.read()
-    md5_returned = hashlib.md5(data).hexdigest()
-    return md5_returned
+    with open(file_name, 'rb') as file_to_check:
+        data = file_to_check.read()
+        md5_returned = hashlib.md5(data).hexdigest()
+        return md5_returned
+
 
 def getRealPath(path):
     return os.path.expanduser(path)
 
-def writeLog(logtype, msg):
-  if logtype == 'd':
-    logging.debug(msg)
-  elif logtype == 'i':
-    logging.info(msg)
-  elif logtype == 'w':
-    logging.warning(msg)
+
+def writeLog(logtype, msg, sprint=True):
+    if logtype == 'd':
+        logging.debug(msg)
+    elif logtype == 'i':
+        logging.info(msg)
+    elif logtype == 'w':
+        logging.warning(msg)
+    else:
+        logging.warning('Unrecognized logtype given to writeLog function!')
+
+    if sprint:
+        print(msg)
+
+
+def loadPlugin(uri, absl=False):
+    if not absl:
+        uri = os.path.normpath(os.path.join(os.path.dirname(__file__), uri))
+    path, fname = os.path.split(uri)
+    mname, ext = os.path.splitext(fname)
+
+    no_ext = os.path.join(path, mname)
+
+    if os.path.exists(no_ext + '.pyc'):
+        try:
+            return imp.load_compiled(mname, no_ext + '.pyc')
+        except:
+          writeLog('w', 'Failed loading plugin: %s', (mname))
+    pass
+
+
+    if os.path.exists(no_ext + '.py'):
+        try:
+            return imp.load_source(mname, no_ext + '.py')
+        except:
+          writeLog('w', 'Failed loading plugin: %s', (mname))
+    pass
+
+
+def loadConf(loc):
+  global config
+  if os.path.isfile(loc):
+    execfile(loc, config)
   else:
-    logging.warning('Unrecognized logtype given to writeLog function!')
+    print("%s wasn't found.. exiting.." % (loc))
+    exit()
 
-  print(msg)
-
-
-def loadPlugin(self, uri, absl=False):
-	if not absl:
-		uri = os.path.normpath(os.path.join(os.path.dirname(__file__), uri))
-	path, fname = os.path.split(uri)
-	mname, ext = os.path.splitext(fname)
-
-	no_ext = os.path.join(path, mname)
-
-	if os.path.exists(no_ext + '.pyc'):
-		try:
-			return imp.load_compiled(mname, no_ext + '.pyc')
-		except:
-			pass
-	if os.path.exists(no_ext + '.py'):
-		try:
-			return imp.load_source(mname, no_ext + '.py')
-		except:
-			pass
+  return config
